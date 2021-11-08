62a946e048c2ef82f59137232cba7f993b14f3e8
ShuGo <schw4one1win@gmail.com>
2021-11-08 23:07:24 +0900

    Implement Author

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_03_081055) do
+ActiveRecord::Schema.define(version: 2021_11_08_134733) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "memo"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "author"
   end
 
 end
