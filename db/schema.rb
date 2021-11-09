07de6d68ed43693faaaa781e9e259c0bbcfb31ba
ShuGo <schw4one1win@gmail.com>
2021-11-09 23:08:17 +0900

    Implement uploader Picture

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_08_134733) do
+ActiveRecord::Schema.define(version: 2021_11_09_091403) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_11_08_134733) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "author"
+    t.string "picture"
   end
 
 end
